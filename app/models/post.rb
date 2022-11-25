51153fc9df069c99c02a92a776c1ce95e4e561ac
Steve <steve.maina@student.moringaschool.com>
2022-11-25 15:23:43 +0300

    Complete assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+  end
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+    validate :click?
+  
+    STRINGS = [/Secret/i, /Won't Believe/i, /Guess/i, /Top \d/i]
+    def click?
+      if STRINGS.none? { |t| t.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+  end
